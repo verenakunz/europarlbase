1624b3020c072f392db8c2f3077980dbc397c84f
Verena Kunz <vkunz@mail.uni-mannheim.de>
2024-08-20 12:09:07 +0200

    Update functions

---
+++ R/get_debate_contributions.R
@@ -0,0 +1,9 @@
+#' Get debate contributions
+#'
+#' `get_debate_contributions()` gets the debate data from EuroParlBase.
+#' @param
+#' @return A data frame of debate contributions and corresponding meta data.
+
+get_debate_contributions <- function() {
+
+}
