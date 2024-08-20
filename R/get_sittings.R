ba996efd1fcf4870ff133e8a5ab9c8bd2e328625
Verena Kunz <vkunz@mail.uni-mannheim.de>
2024-08-20 15:55:06 +0200

    Update get_sittings()

--- R/get_sittings.R
+++ R/get_sittings.R
@@ -5,6 +5,11 @@
 
 get_sittings <- function(key = access_token) {
 
+  ## Check if access token object exists in environment
+  if(exists(access_token) == FALSE) {
+    stop ("Have you specified the access token?")
+  }
+
   ## Build URL to sitting data
   url <- paste0("https://dataverse.harvard.edu/api/access/datafile/10247109?key=", key)
 
