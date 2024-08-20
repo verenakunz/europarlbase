a01f91e0e407fe6e40b4d457dc17bf02fe9dcbd8
Verena Kunz <vkunz@mail.uni-mannheim.de>
2024-08-20 13:17:39 +0200

    Update functions

--- R/get_debate_contributions.R
+++ R/get_debate_contributions.R
@@ -3,7 +3,8 @@
 #' `get_debate_contributions()` gets the debate data from EuroParlBase.
 #' @param
 #' @return A data frame of debate contributions and corresponding meta data.
+#' @export
 
-get_debate_contributions <- function() {
+get_debate_contributions <- function(access_token = access_token) {
 
 }
---
+++ R/get_meps.R
@@ -0,0 +1,8 @@
+#' Get socio-demographic MEP data
+#'
+#' `get_meps()` gets socio-demographic MEP data from EuroParlBase.
+#' @export
+
+get_meps <- function(access_token = access_token) {
+
+}
