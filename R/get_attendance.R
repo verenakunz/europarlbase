97a0175e90b12bb08a30b32e87acd7acfac02c7c
Verena Kunz <vkunz@mail.uni-mannheim.de>
2024-10-31 00:30:51 +0100

    Update files

---
+++ R/get_attendance.R
@@ -0,0 +1,25 @@
+#' Get attendance data
+#'
+#' `get_attendance()` gets the attendance data from EuroParlBase.
+#' @export
+
+get_attendance <- function(access_token = NULL) {
+
+  ## Check if access token object exists in user's global environment
+  if (is.null(access_token)) {
+    if (exists("access_token", envir = .GlobalEnv)) {
+      access_token <- get("access_token", envir = .GlobalEnv)
+    } else {
+      stop("\n\nAccess token is missing. Have you specified the access token correctly? Check the package documentation for information on how to specify the access token.")
+    }
+  }
+
+  ## Build URL to sitting data
+  url <- paste0("https://dataverse.harvard.edu/api/access/datafile/10676743?key=", access_token)
+
+  ## Read data
+  connect <- url(url)
+  on.exit(close(connect))
+  data <- readRDS(connect)
+  return(data)
+}
